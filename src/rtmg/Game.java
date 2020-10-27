c2b29be01aa135b7a31346f9632fe893e34dc82c
manjitsaha <manjitsaha01@gmail.com>
2020-10-27 18:33:05 +0530

    added dummy tiles and added key input for movement

--- src/rtmg/Game.java
+++ src/rtmg/Game.java
@@ -4,46 +4,55 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import rtmg.input.Keyboard;
 import rtmg.screen.Screen;
 
-public class Game extends Canvas implements Runnable{
-	
+public class Game extends Canvas implements Runnable {
+
 	private static final long serialVersionUID = 1L;
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
-	
+	public static String title = "RTMG";
+
 	private Thread thread;
 	private JFrame frame;
+	private Keyboard key;
 	private boolean running = false;
-	
+
 	private Screen screen;
-	
-	private BufferedImage image = new BufferedImage(width , height , BufferedImage.TYPE_INT_RGB);
+
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
-	
+
+	int x = 0, y = 0;
+
 	public Game() {
-		
-		Dimension size = new Dimension(width * scale , height * scale);
+
+		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
-		
-		screen = new Screen(width , height);
+
+		screen = new Screen(width, height);
 		frame = new JFrame();
-		
+		key = new Keyboard();
+		addKeyListener(key);
+
 	}
-	
+
 	public synchronized void start() {
 		running = true;
-		thread = new Thread(this , "Display");
+		thread = new Thread(this, "Display");
 		thread.start();
 	}
-	
+
 	public synchronized void stop() {
 		running = false;
 		try {
@@ -56,59 +65,87 @@ public class Game extends Canvas implements Runnable{
 
 	@Override
 	public void run() {
-		
-		while(running) {
-			
-			update();
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+
+			while (delta >= 1) {
+				update();
+				updates++;
+				delta--;
+			}
+
 			render();
-			
+			frames++;
+
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				frame.setTitle(Game.title + "  |  " + "Updates " + updates + " ups " + frames + " fps");
+				updates = 0;
+				frames = 0;
+			}
+
 		}
-		
-		
+		stop();
+
 	}
-	
+
 	private void render() {
 		BufferStrategy bs = getBufferStrategy();
-		if(bs == null) {
+		if (bs == null) {
 			createBufferStrategy(3);
-			return; 
+			return;
 		}
-		
+
 		screen.clear();
-		screen.render();
-		
-		for(int i = 0 ; i < pixels.length ; i++) {
+		screen.render(x, y);
+
+		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = screen.pixels[i];
 		}
-		
+
 		Graphics g = bs.getDrawGraphics();
-		
+
 		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
-		
+
 		g.dispose();
 		bs.show();
-		
+
 	}
 
 	private void update() {
-		 
+		key.update();
+		if (key.up) y--;
 		
+		if (key.down) y++;
+		
+		if (key.left) x--;
+		
+		if (key.right) x++;
+		
+
 	}
 
 	public static void main(String[] args) {
-		
+
 		Game game = new Game();
 		game.frame.setResizable(false);
-		game.frame.setTitle("RTMG");
+		game.frame.setTitle(Game.title);
 		game.frame.add(game);
 		game.frame.pack();
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		game.frame.setLocationRelativeTo(null);
 		game.frame.setVisible(true);
-		
+
 		game.start();
-		
+
 	}
-	
+
 }
- 
